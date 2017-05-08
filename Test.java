4fd06af521ebb291090d7c890db8b6fbb4ed8652
Thomas Dunajski <thomas.dunajski@gfnork.de>
2017-05-08 09:17:24 +0200

    added webserver integration still missing

---
+++ Test.java
@@ -0,0 +1,42 @@
+package xgb.st.de;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/kuehlschrank/history", new HistoryHandler());
+        server.createContext("/kuehlschrank/fuellstand", new HistoryHandler());
+        server.start();
+    }
+
+    static class HistoryHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "TODO: History hier ausgeben";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+    
+    static class FuellstandHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "TODO: Füllstand hier ausgeben";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+}
