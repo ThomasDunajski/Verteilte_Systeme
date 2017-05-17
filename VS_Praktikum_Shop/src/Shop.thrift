d6acd8e80307024b61599dd21a6710fc014dc04e
Thomas Dunajski <thomas.dunajski@gfnork.de>
2017-05-17 14:21:28 +0200

    initial commit added thrift file and generated java classes

---
+++ VS_Praktikum_Shop/src/Shop.thrift
@@ -0,0 +1,30 @@
+/**
+ * The available types in Thrift are:
+ *
+ *  bool        Boolean, one byte
+ *  i8 (byte)   Signed 8-bit integer
+ *  i16         Signed 16-bit integer
+ *  i32         Signed 32-bit integer
+ *  i64         Signed 64-bit integer
+ *  double      64-bit floating point value
+ *  string      String
+ *  binary      Blob (byte array)
+ *  map<t1,t2>  Map from one type to another
+ *  list<t1>    Ordered list of one type
+ *  set<t1>     Set of unique elements of one type
+ *
+ */
+
+namespace java RPC
+
+struct Order {
+  1: i32 amount,
+  2: string product
+}
+
+service Shop{
+
+   bool placeOrder(1:Order order),
+
+   double getPrice(1:string name) 
+}
